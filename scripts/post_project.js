e69971dc51a2221a4e5895816aae7f5a8ed82288
razzanYoni <16521378@mahasiswa.itb.ac.id>
2022-08-14 16:35:07 +0700

    feat:Add styling

---
+++ scripts/post_project.js
@@ -0,0 +1,34 @@
+/* Input file*/
+const form = document.querySelector("form") ;
+fileInput = form.querySelector(".file-input") ;
+
+form.addEventListener("click", ()=> {
+    fileInput.click();
+
+})
+
+/* display file */
+var loadFile = function(event) {
+    var output = document.getElementById("output");
+    output.src = URL.createObjectURL(event.target.files[[0]]);
+    output.onload = function() {
+        URL.revokeObjectURL(output.src)
+    }
+}
+
+
+
+// fileInput.onchange = ({target}) => {
+//     let file = target.files[0];
+//     if(file) {
+//         let fileName = file.name;
+//         uploadFile(fileName);
+//     }
+// 
+// }
+// 
+// function uploadFile(name) {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("POST",);
+//     xhr.send()
+// }
